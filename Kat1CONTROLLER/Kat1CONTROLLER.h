6d4c212166f3f677dfde7d9ce0800d701fddcb1c
giancarlozn <g.zamudio@pucp.edu.pe>
2025-10-05 19:01:54 -0500

    lista para ensayos y encabezado de los metodos

--- Kat1CONTROLLER/Kat1CONTROLLER.h
+++ Kat1CONTROLLER/Kat1CONTROLLER.h
@@ -9,6 +9,8 @@ namespace Kat1CONTROLLER {
 	public ref class Controller
 		{
 		private:
+			//LISTA ENSAYOS
+			static List<Ensayo^>^ ensayos = gcnew List<Ensayo^>();
 			//LISTA PLANTAS
 			static List<Planta^>^ plantas = gcnew List<Planta^>();
 			//LISTA ROBOTS
@@ -16,6 +18,13 @@ namespace Kat1CONTROLLER {
 			//LISTA USUARIOS
 			static List<Usuario^>^ usuarios = gcnew List<Usuario^>();
 		public:
+			//ensayo
+			static int AgregarEnsayo(Ensayo^ Ensayo);
+			static Ensayo^ ConsultaEnsayo(int EnsayoID);
+			static List<Ensayo^>^ ObtenerEnsayo();
+			static int EliminarEnsayo(int EnsayoID);
+			static int ModificarEnsayo(Ensayo^ Ensayo);
+
 			//plantas
 			static int AgregarPlanta(Planta^ Planta);
 			static Planta^ ConsultaPlanta(int PlantaID);
